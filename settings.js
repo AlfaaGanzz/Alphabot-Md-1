4a980c5fb28730e0d336857fd9f413c82fd36663
Smart Bot々 <88409247+AlfaaGanzz@users.noreply.github.com>
2023-04-15 12:51:34 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -47,16 +47,16 @@ global.language = indonesia //change indonesia to english if you don't understan
 // Other
 // Other
 global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.ownername = "X-Faa" //nama kalian
+global.myweb = "-" //bebas asal jan hapus
+global.youtube = "https://youtube.com/@Alfrp29_" //bebas asal jan hapus
+global.myweb2 = "-" //bebas
+global.email = "putrantinoa@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
+global.premium = ['6285704682918'] //premium user
+global.owner = ["6285704682918", "62896299520593"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6285704682918" // nomor wa kalian
 global.ownernomerr = "+"+ownernomer //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
